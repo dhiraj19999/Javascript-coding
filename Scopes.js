f0a474fd4718059aa87ab7ed53a8c995cc1dc3b8
dhiraj19999 <yogeshsamse682@gmail.com>
2022-08-30 22:48:38 +0530

    added scope

--- Scopes.js
+++ Scopes.js
@@ -7,3 +7,34 @@
 //In JavaScript, objects and functions are also variables.
 // Scope determines the accessibility of variables, objects,
 // and functions from different parts of the code.
+
+
+
+// What is the execution context & stack in JavaScript?//
+
+//The Execution Context is an abstract environment created by JS to execute code.
+// The Execution Stack is used to execute functions and keep track of all the 
+//Execution Context created. The Execution context is created in two stages, 
+//the creation and the execution stage
+
+//he Execution Context contains the code that's currently running, and everything 
+//that aids in its execution. During the Execution Context run-time, the specific 
+//code gets parsed by a parser, the variables and functions are stored in memory, 
+//executable byte-code gets generated, and the code gets executed.
+
+
+
+
+
+
+
+// GLOBAL SCOPE//
+
+//In a programming environment, the global scope is the scope that contains, 
+//and is visible in, all other scopes. In client-side JavaScript, 
+//the global scope is generally the web page inside which all the code is 
+//being executed.
+//Global variables can be accessed and modified anywhere in the program
+
+
+
